03235c30babf3f239792de33bbcd20aa11958a25
i-amdash <ashimidoyin@yahoo.com>
2024-03-23 09:39:17 +0100

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller {
+    public function index()
+    {
+        return [
+            "name" => "this is for doyin"
+        ];
+    }
+}
